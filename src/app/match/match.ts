dba4e70f8a06d29e70e1bb77f7d52f8f48c83442
Suryaprakash1024 <suryaprakash1024999@gmail.com>
2021-02-26 17:40:49 +0530

    match model added

---
+++ src/app/match/match.ts
@@ -0,0 +1,12 @@
+export class Match {
+  key: string;
+  name: string;
+  amount: number;
+  team: string;
+  paymentmethod: string;
+  ratio: number;
+  number: number;
+  winningAmount: number;
+  paid: boolean;
+  isDeleted: boolean;
+}
